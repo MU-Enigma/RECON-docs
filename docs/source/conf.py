423ea53cad1a1f8e182b2a60df559bbc7078ca68
0xd1r <92980894+0xd1r@users.noreply.github.com>
2022-06-14 15:39:07 +0530

    Initialize conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RECON'
+copyright = '2022, MU-Enigma'
+author = 'MU-Enigma'
 
 release = '0.1'
 version = '0.1.0'
